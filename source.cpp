f13560f9366455e918cd1c7d577a561b914c26f7
Michael <xenophon.of.athens@gmail.com>
2015-12-10 20:05:25 -0700

    commit 2

--- source.cpp
+++ source.cpp
@@ -9,13 +9,19 @@ class PageSequence : std::deque<int>
 
 int main()
 {
-	std::deque<int> memory[100];
-	for (int i = 0; i < 100; ++i)
+	for (int q = 0; q < 100; ++q)
 	{
-		for (int j = 0; j < 1000; ++j)
+		std::deque<int> memory[100];
+		for (int i = 0; i < 100; ++i)
 		{
-			int k = rand() % 250 + 1;
-			memory[i].push_back(k);
+			for (int j = 0; j < 1000; ++j)
+			{
+				int k = rand() % 250 + 1;
+				memory[i].push_back(k);
+				std::cout << k << " ";
+			}
+			std::cout << "\n\n";
 		}
+		std::cout << "\n\n\n\n";
 	}
 }
